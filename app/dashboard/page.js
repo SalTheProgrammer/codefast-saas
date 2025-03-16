bd7bae520f9e1784bdadef946918ae3da58565f0
SalTheProgrammer <faisal.sudradjat@gmail.com>
2025-03-16 15:22:27 +0800

    add hero & header section

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
     <main>
-      <h1>Privare Dashboard</h1>
+      <h1>Private Dashboard</h1>
     </main>
   );
 }
--- app/page.js
+++ app/page.js
@@ -2,18 +2,33 @@ import ButtonLogin from "@/components/ButtonLogin";
 
 export default function Home() {
   const isLoggedIn = true;
-  const name = "Blah";
+  const name = "Sal";
   return (
     <main>
-      <h1 className="text-3xl font-bold p-4">
-        Collect customer feedback to build better product
-      </h1>
-      <div>
-        Collect a feedback board in minutes, prioritize features, and build
-        products your customers will love.
-      </div>
+      <section className="bg-base-200">
+        <div className="max-w-3xl mx-auto  flex justify-between items-center px-8 py-2">
+          <div className="font-bold">CodeFastSaaS</div>
+          <div className="space-x-4 max-md:hidden">
+            <a className="link link-hover">Pricing</a>
+            <a className="link link-hover">FAQ</a>
+          </div>
+          <div>
+            <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+          </div>
+        </div>
+      </section>
 
-      <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      <section className="text-center px-8 py-32 max-w-3xl mx-auto">
+        <h1 className="lg:text-5xl text-4xl font-extrabold mb-6">
+          Collect customer feedback to build better product
+        </h1>
+        <div className="opacity-90 mb-10">
+          Collect a feedback board in minutes, prioritize features, and build
+          products your customers will love.
+        </div>
+
+        <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      </section>
     </main>
   );
 }
