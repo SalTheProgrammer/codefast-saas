6c780a925255fc0f25dec60a137e632f11b38507
SalTheProgrammer <faisal.sudradjat@gmail.com>
2025-03-16 12:23:33 +0800

    added some styling

---
+++ app/dashboard/page.js
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <main>
+      <h1>Privare Dashboard</h1>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
-import Image from "next/image";
+import ButtonLogin from "@/components/ButtonLogin";
 
 export default function Home() {
+  const isLoggedIn = true;
+  const name = "Blah";
   return (
     <main>
-      <h1>Collect customer feedback to build better product</h1>
+      <h1 className="text-3xl font-bold p-4">
+        Collect customer feedback to build better product
+      </h1>
       <div>
         Collect a feedback board in minutes, prioritize features, and build
         products your customers will love.
       </div>
+
+      <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
     </main>
   );
 }
